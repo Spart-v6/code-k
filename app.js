db758f21cea62a7279537fa47f2a4dbda02d63b1
Ankur-S1 <65460931+Ankur-S1@users.noreply.github.com>
2020-07-18 08:50:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// for navbar while scrolling
+const header = document.querySelector('.header');
+window.onscroll= function(){
+    var top = window.scrollY;
+    // console.log(top);
+    if(top>=100){
+        header.classList.add('active');
+    }
+    else{
+        header.classList.remove('active')
+    }
+}
